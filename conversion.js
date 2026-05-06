36f4d5fdbd19548e11de7cf5a6c09d0439aa2ece
Deepak Gupta <deepakgupta190603@gmail.com>
2026-05-06 18:55:15 +0000

    variables and conversion

---
+++ conversion.js
@@ -0,0 +1,16 @@
+let a="10";
+let b ='10abc'
+let e=null
+str="Deepak"
+z=true
+d=Number(a);
+c=Number(b);
+f=Number(e);
+number=100
+
+
+console.log(String(number))
+console.log(d)
+console.log(c);
+console.log(f)
+console.log(Number(z))
---
+++ variables.js
@@ -0,0 +1,18 @@
+const b=20
+let a=10;
+var c=90
+str="deepak"
+
+console.log(b);
+console.log(a);
+console.log(c);
+console.log(str);
+d=null
+str;
+
+console.log(typeof c);
+console.log(typeof a);
+console.log(typeof d)
+console.log(typeof str)
+
+
