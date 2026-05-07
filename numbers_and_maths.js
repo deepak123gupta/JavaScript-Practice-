e370d93cb7008053760ca1e7f589f0fe8103dd95
Deepak Gupta <deepakgupta190603@gmail.com>
2026-05-07 09:47:03 +0000

    math and number in js

---
+++ numbers_and_maths.js
@@ -0,0 +1,34 @@
+let num=100;
+// console.log(num)
+
+let num1=new Number(400);
+// console.log(num1);
+
+// console.log(num1.toFixed(2));
+
+let num2=new Number(123.45)
+
+//It will return a string
+// console.log(num2.toPrecision(4));
+
+const hundered=100000
+// console.log(hundered.toLocaleString());
+// console.log(hundered.toLocaleString('en-In'));
+
+//++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(5.4))
+// console.log(Math.floor(4.2))
+// console.log(Math.round(4.8))
+
+// console.log(Math.max(4,5,6,6,7,8,9))
+console.log(Math.min(3,4,5,9,2));
+
+// console.log(Math.random())
+// console.log((Math.random()*10) +1)
+let min=10
+let max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
